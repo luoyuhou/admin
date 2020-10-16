32fe837becb3c3a4d5c7c32676968479ea4944c2
ruhouluo <ruhouluo@xmhkj2020.com>
2020-10-16 14:55:31 +0800

    update sql

--- application/admin/command/Install/table.sql
+++ application/admin/command/Install/table.sql
@@ -53,7 +53,20 @@ CREATE TABLE `fa_course_user_process` (
 ) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='课程列表';
 
 ALTER TABLE `fastadmin`.`fa_course`
-MODIFY COLUMN `status` varchar(10) NULL DEFAULT 1 COMMENT '状态' AFTER `title`;
+MODIFY COLUMN `status` varchar(10) NULL DEFAULT 'normal' COMMENT '状态' AFTER `title`;
 ALTER TABLE `fastadmin`.`fa_course_type`
 MODIFY COLUMN `admin_id` int(10) UNSIGNED NULL DEFAULT NULL COMMENT '管理员' AFTER `id`,
 MODIFY COLUMN `user_id` int(10) UNSIGNED NULL DEFAULT NULL COMMENT '会员' AFTER `admin_id`;
+
+CREATE TABLE `fa_upload` (
+    `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+    `name` varchar(50) NOT NULL COMMENT '文件名称',
+    `size` int(10) unsigned NOT NULL COMMENT '课程标题',
+    `ip` varchar(50) NOT NULL COMMENT '会员',
+    `agent` varchar(255) COMMENT '',
+    `type` varchar(50) COMMENT '类型',
+    `hash` varchar(50) COMMENT '哈希值',
+    `createtime` int(10) DEFAULT NULL COMMENT '创建时间',
+    primary key (`id`),
+    unique key `hash` (`hash`)
+) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='上传文件表';
